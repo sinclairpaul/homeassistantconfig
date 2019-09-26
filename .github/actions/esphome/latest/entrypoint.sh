2241b41caf287e8a902125a528bdabbb45c90756
Paul Sinclair <sinclairpaul@hotmail.com>
2019-09-26 11:09:17 -0400

    🔨Test script

--- .github/actions/esphome/latest/entrypoint.sh
+++ .github/actions/esphome/latest/entrypoint.sh
@@ -4,12 +4,11 @@ set -e
 
 sh -c "esphome dummy.yaml version"
 sh -c "cp ci_secrets.yaml ./esphome/secrets.yaml"
-sh -c "|
-        for file in $(find \
+sh -c "for file in $(find \
         ./esphome \
         -type f \
-        -name "*.yaml"\
-        -not -name "secrets.yaml"
+        -name "*.yaml" \
+        -not -name "secrets.yaml" \
         ); do
         esphome $(file) config
       done"
