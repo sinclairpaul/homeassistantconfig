9d55654f510455bf7f937ca8172014cdbe5b05ca
Paul Sinclair <sinclairpaul@hotmail.com>
2019-09-26 11:02:57 -0400

    🔨Update script

--- .github/actions/esphome/latest/entrypoint.sh
+++ .github/actions/esphome/latest/entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 sh -c "esphome dummy.yaml version"
 sh -c "cp ci_secrets.yaml ./esphome/secrets.yaml"
-sh -c "for file in $(find \
+sh -c "|
+        for file in $(find \
         ./esphome \
         -type f \
         -name "*.yaml"\
